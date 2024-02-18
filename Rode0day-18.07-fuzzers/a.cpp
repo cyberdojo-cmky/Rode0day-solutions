608ac9d77002a69b4c7d04b0aa5d032b657acc75
kn0 <vladvatafu9@gmail.com>
2024-02-18 06:06:13 +0200

    Update a.cpp

--- Rode0day-18.07-fuzzers/a.cpp
+++ Rode0day-18.07-fuzzers/a.cpp
@@ -6,6 +6,46 @@
 #include <cstring>
 #include <cassert>
 #include <random>
+#include <cstdint>
+
+using namespace std;
+
+class FileMutator {
+  using V = std::vector<uint8_t>;
+
+  public:
+  
+  private:
+    void Append(V *to, const V &from) {
+      to->insert(to->end(), from.begin(), from.end());
+    }
+    void PrintHex(const V &v, size_t max_n) {
+        for (size_t i = 0; i < max_n && i < v.size(); i++) {
+          std::cerr << "0x" << std::hex << (unsigned)v[i] << " " << std::dec;
+        }
+        std::cerr << std::endl;
+      }
+    uint32_t Read4(std::istream &in) {
+        uint32_t res = 0;
+        in.read((char *)&res, sizeof(res));
+        return res;
+      }
+      uint32_t ReadInteger(std::istream &in) {
+        return __builtin_bswap32(Read4(in));
+      }
+      static uint32_t Type(const char *tagname) {
+        uint32_t res;
+        assert(strlen(tagname) == 4);
+        memcpy(&res, tagname, 4);
+        return res;
+      }
+    
+      void WriteInt(std::ostream &out, uint32_t x) {
+        x = __builtin_bswap32(x);
+        out.write((char *)&x, sizeof(x));
+      }
+
+};
 
 extern "C" size_t LLVMFuzzerCustomMutator(uint8_t *Data, size_t Size,
                                           size_t MaxSize, unsigned int Seed) {
